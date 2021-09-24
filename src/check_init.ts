32368287565cd3b8bf2fee90e908422435ed99ac
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-09-24 18:01:07 +0700

    Added files

---
+++ src/check_init.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { API_BASE, ADDRESS } from './config';
+
+axios.get(API_BASE + "/addresses/data/" + ADDRESS + "?key=available_punks")
+    .then(res => {
+        let value = res.data[0].value;
+        let arr = value.split(",");
+        for (let i = 0; i <= 1337; i ++) {
+            if (arr[i] != i) {
+                console.log("Check failed!");
+                console.log(arr[i] + " != " + i);
+                break;
+            }
+        }
+    })
+    .catch(e => {
+        console.log(e);
+    })
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const CHAIN_ID = 'T'
+export const API_BASE = 'https://nodes-testnet.wavesnodes.com';
+export const ADDRESS = '';
+export const SEED = '';
+
+export const TIMEOUT = 60000;
